0c78bf5fed989a9801cb796657295a1738f306a8
mazunki <rolferen@gmail.com>
2019-05-25 15:27:35 +0200

    .

--- HunderGames.py
+++ HunderGames.py
@@ -5,7 +5,7 @@ led1 = GPIO.setup(2, GPIO.OUT)
 button1 = GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-	if GPIO.input(2)
+	if GPIO.input(2):
 		print("HIGH")
 	else:
 		print("LOW")
