efe2b6fb6792880088bea16fe206bd9d17f2e963
mazunki <rolferen@gmail.com>
2019-05-25 15:36:02 +0200

    quick update

--- HunderGames.py
+++ HunderGames.py
@@ -4,7 +4,7 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 
 led1 = GPIO.setup(2, GPIO.OUT)
-button1 = GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+button1 = GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
 	if GPIO.input(2):
