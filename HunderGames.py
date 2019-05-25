b928e7fa01a40565d12928a2866377b2d4dee58a
mazunki <rolferen@gmail.com>
2019-05-25 15:27:12 +0200

    .

--- HunderGames.py
+++ HunderGames.py
@@ -1,2 +1,12 @@
 import RPi.GPIO as GPIO
+from time import sleep
 
+led1 = GPIO.setup(2, GPIO.OUT)
+button1 = GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+	if GPIO.input(2)
+		print("HIGH")
+	else:
+		print("LOW")
+	sleep(1)
