d12c14123ede6e817c91d76e5fa707c67880fc9b
mazunki <rolferen@gmail.com>
2019-05-25 15:33:32 +0200

    quick update

--- HunderGames.py
+++ HunderGames.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 led1 = GPIO.setup(2, GPIO.OUT)
 button1 = GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
